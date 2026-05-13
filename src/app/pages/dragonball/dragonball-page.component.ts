2a2137f4dd049dd866248c887003a532b24b7697
gpena87 <gpenagutierrez87@gmail.com>
2026-05-12 20:26:59 -0400

    fix dragon ball

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,8 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
     selector: 'app-dragonball-page',
     templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+
+  name = signal('Gohan');
+  power = signal(100);
+
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 4, name: 'Piccolo', power: 3000 },
+    { id: 3, name: 'Yamcha', power: 500 },
+  ])
+
+  addCharacter(): void {
+    if (!this.name() || this.power() <= 0) return;
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+
+    this.characters.update(characters => [...characters, newCharacter]);
+  }
+
 }
